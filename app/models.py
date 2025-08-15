8a85b5dc056b965a85dc43dc2b2816473b0b58e9
Frogguzzz <ranchow3@gmail.com>
2025-08-15 08:20:38 -0300

    Models Create Commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Usuario(AbstractUser):
+
+    ## Atributes ##
+    cpf = models.CharField(max_length=11, null=False, blank=False)
+
+    ## Methods ##
+    @classmethod
+    def createUser(cls, username, password, email, cpf):
+        return cls.objects.create_user(username=username, password=password, email=email, cpf=cpf)
+
+class Logo(models.Model):
+
+    ## Atributes ##
+    turma = models.CharField(max_length=40, null=False, blank=False)
+    imagem = models.FileField(null=False, blank=False)
+
+    ## Methods ##
+    @classmethod
+    def createLogo(cls, turma, imagem):
+        return cls.objects.create(turma=turma, imagem=imagem)
+    
+    #
+    @classmethod
+    def editLogo(cls, id, turma, imagem):
+
+        ## Getting object ##
+        logo = cls.objects.get(id=id)
+
+        ## Editing the object ##
+        logo.turma = turma
+        logo.imagem = imagem
+
+        ## Saving the object ##
+        logo.save()
+
+    #
+    @classmethod
+    def deleteLogo(cls, id):
+        return cls.objects.get(id=id).delete()
+    
+class Like(models.Model):
+
+    ## Atributes ##
+    logo = models.ForeignKey(Logo, on_delete=models.CASCADE)
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    ## Methods ##
+    @classmethod
+    def giveLike(cls, user, logo):
+        return cls.objects.create(logo=logo, user=user)
